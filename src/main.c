d9f37a4c90c485c1a2770d3f90ade6bd7b9e3bc0
YarenSarac <saracyaren9@gmail.com>
2021-05-05 23:57:56 +0300

    Düzeltmeler

--- src/main.c
+++ src/main.c
@@ -6,12 +6,12 @@
 
 int main(int argc, char **argv)
 { 
+
   JRB b;
   JRB bn;
   IS is;
   char *p;
   char *ptr;
-  int counter=0;
 
 if (argc != 2) { fprintf(stderr, "usage: $> kripto -d giriş_metin cikis_metin\n"); exit(1); }
 
@@ -24,20 +24,65 @@ if (argc != 2) { fprintf(stderr, "usage: $> kripto -d giriş_metin cikis_metin\n
      if(is->NF!=2)
         
       continue;  
-     p= strdup(is->fields[0]+1);                    
-     p[strlen(p)-2] = '\0';
-     ptr= strdup(is->fields[1]+1);                    
-     ptr[strlen(ptr)-2] = '\0';
+     p= strdup(is->fields[0]);  
+     ptr= strdup(is->fields[1]);      
+     char control=p[0];
+     char control2=ptr[0];
+
+    
+     if(strcmp(&control,"\""))           
+     {
+       
+      printf( "HATALİ KULLANİM\n");
+       exit(1);
+
+     }
+
+     else if(strcmp(&p[strlen(p)-2],"\":"))
+     {
+
+       printf( "HATALİ KULLANİM\n");
+       exit(1);
+     }
+      
+      /* else if(strcmp(&control2,"\""))
+     {
+       
+      printf( "HATALİ KULLANİM\n");
+       exit(1);
+
+     }*/
+     /*else if(strcmp(&ptr[strlen(ptr)-2],"\","))
+     {
+       
+      printf( "HATALİ KULLANİM\n");
+       exit(1);
+
+     }*/
+
+    else
+    {
+       p++;
+       p[strlen(p)-2] = '\0';
+       ptr++;                 
+       ptr[strlen(ptr)-2] = '\0';
+    }
+     
+     
   	// printf("%s\n",p);
-    (void) jrb_insert_str(b, p, new_jval_s(ptr));
-   // free(p);
-   // free(ptr);
+    
+
+    printf("kelime değeri : %s  kelime : %s  değer: %s\n",&control2,p,ptr);
+
+   // (void) jrb_insert_str(b, p, new_jval_s(ptr));
+    //free(p);
+    //free(ptr);
   }
-    jrb_traverse(bn, b) {
+
+   /* jrb_traverse(bn, b) {
     printf("%s\t", bn->key.s);
     printf("%s\n", bn->val.s); 
-  }
-  // EN SON AĞACI SİLMEYİ UNUTMA.
-
+  }*/
+  // EN SON AĞACI SİLMEYİ UNUTMA
   return 0;
 }
